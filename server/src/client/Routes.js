c61842969c8738e7534b183289c8be74f1c97f17
Naveen-Gupta <nvngupta1992@gmail.com>
2020-04-15 00:35:57 +0530

    container for userslist

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Home from './components/Home';
+import Home from "./components/Home";
+import UsersList from "./components/UsersList";
 
 export default () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/users" component={UsersList} />
+    </div>
+  );
+};
---
+++ server/src/client/components/UsersList.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../actions";
+
+class UsersList extends Component {
+  componentDidMount() {
+      this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map((user) => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ users }) => {
+  return { users };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
