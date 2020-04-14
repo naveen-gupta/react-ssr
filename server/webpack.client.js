88a50184e2965d0077534b0ec202bf6f68f884f0
Naveen-Gupta <nvngupta1992@gmail.com>
2020-04-14 22:53:16 +0530

    code refactored: merge webpack config, merge npm scripts

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,27 +1,18 @@
-const path = require('path');
+const path = require("path");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
-	// Tell webpack the root file of our server app
-	entry: './src/client/client.js',
+const config = {
+  // Tell webpack the root file of our server app
+  entry: "./src/client/client.js",
 
-	// Tell webpack where to put the output file
-	// that is generated
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public'),
-	},
-
-	// Tell webpack to run babel on every file it runs through
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: ['react', 'stage-0', ['env', { targets: { browsers: ['last 2 versions'] } }]],
-				},
-			},
-		],
-	},
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
 };
+
+module.exports = merge(baseConfig, config);
+
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,31 +1,21 @@
-const path = require('path');
+const path = require("path");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
-	// Inform webpack that we're building a bundle
-	// for nodeJS, rather than for the browser
-	target: 'node',
+const config = {
+  // Inform webpack that we're building a bundle
+  // for nodeJS, rather than for the browser
+  target: "node",
 
-	// Tell webpack the root file of our server app
-	entry: './src/index.js',
+  // Tell webpack the root file of our server app
+  entry: "./src/index.js",
 
-	// Tell webpack where to put the output file
-	// that is generated
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build'),
-	},
-
-	// Tell webpack to run babel on every file it runs through
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: ['react', 'stage-0', ['env', { targets: { browsers: ['last 2 versions'] } }]],
-				},
-			},
-		],
-	},
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
 };
+
+module.exports = merge(baseConfig, config);
