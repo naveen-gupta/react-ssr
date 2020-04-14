3920341e68cefd9b55490eab7756f0028741c9b3
Naveen-Gupta <nvngupta1992@gmail.com>
2020-04-14 23:03:09 +0530

    code refactored: removed webpack-node-externals

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that we're building a bundle
@@ -16,6 +17,8 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
